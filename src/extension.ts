0ba780e9d1011010ac498a1adce6c8c4cdcb62b0
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2016-10-26 19:56:46 +0200

    use object as `DocumentSelector`

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,11 @@
-//
-// Copyright (C) Fabian Lauer, Softwareschmiede Saar.
-// All rights reserved.
-//
-
 import * as vscode from 'vscode';
 import * as xmlFormatter from './xmlFormatter';
 
 export function activate(context: vscode.ExtensionContext) {
 	// whole document formatting
-	context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('xml', {
+	context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider({ language: 'xml' }, {
 		provideDocumentFormattingEdits: (document, options) => {
 			return xmlFormatter.XmlFormatter.format(document, undefined, options);
-		}
-	}));
-	
-	// selection formatting
-	context.subscriptions.push(vscode.languages.registerDocumentRangeFormattingEditProvider('xml', {
-		provideDocumentRangeFormattingEdits: (document, range, options) => {
-			return xmlFormatter.XmlFormatter.format(document, range, options);
 		}
 	}));
 }
